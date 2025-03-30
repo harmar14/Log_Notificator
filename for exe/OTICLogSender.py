b6b161ddbc33aaa81d08a63919ec2fd1282ec48d
Iuliia Belokon <75740030+harmar14@users.noreply.github.com>
2025-03-30 15:31:48 +0300

    Update OTICLogSender

--- for exe/OTICLogSender.py
+++ for exe/OTICLogSender.py
@@ -3,6 +3,7 @@ import sys
 import codecs
 import json
 import smtplib
+import zipfile
 from datetime import datetime, date, time, timedelta
 from pathlib import Path
 from email.mime.multipart import MIMEMultipart
@@ -16,6 +17,7 @@ CONFIG_PATH = "\\".join(EXE_PATH.split('\\')[:-1]) + "\\config.json"
 LOG_PATH = "\\".join(EXE_PATH.split('\\')[:-1]) + "\\logs"
 LOG_NAME = f"{date.today()}.txt"
 LOG_EXTENSIONS = [".xml", ".txt", ".csv"]
+ZIP_NAME = "Logs.zip"
 
 class WriteError:
     
@@ -133,6 +135,60 @@ class FindFreshLogs:
             return
         
         return self.scanDirectory(root, yesterday, [])
+
+class ZipLogs:
+    
+    def __init__(self):
+        self.ok = True
+        self.errMsg = ''
+    
+    def ZipFiles(self, paths):
+        
+        for path in paths:
+            # Запускаем преобразование только тогда, когда есть, что преобразовывать.
+            if (len(path["logs"]) > 0):
+                
+                # Проверяем существование целевой директории.
+                if not (os.path.isdir(path["path"])):
+                    self.ok = False
+                    self.errMsg = f"{path['path']} does not exist."
+                    return
+                
+                # Формируем полный путь к архиву.
+                destination = f"{path['path']}\\{ZIP_NAME}"
+                
+                # Если такой архив уже существует, сначала удаляем существующий.
+                if (os.path.exists(destination)):
+                    try:
+                        os.remove(destination)
+                    except Exception as e:
+                        self.ok = False
+                        self.errMsg = e
+                        return
+                
+                # Открываем zip-файл.
+                with zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+                    # Проходим по всем найденным лог-файлам.
+                    for log_path in path["logs"]:
+                        # Добавляем логи в архив, предварительно проверив, что это файлы.
+                        if (os.path.isfile(log_path)):
+                            zip_file.write(log_path)
+                
+                # Перезаписываем path["logs"] на полученный архив.
+                path["logs"] = [destination]
+        
+        return paths
+    
+    def execute(self, paths):
+        
+        paths = self.ZipFiles(paths)
+        
+        if not (self.ok):
+            # Пишем лог.
+            WriteError().writeLog("ERROR", "ZipLogs:execute", self.errMsg)
+            return
+        
+        return paths
 
 class SendEmail:
     
@@ -329,6 +385,11 @@ class SendEmail:
         self.getLogs()
         
         if not (self.ok):
+            return
+        
+        self.logs = ZipLogs().execute(self.logs)
+        
+        if (self.logs is None):
             return
         
         self.sendLogs()
